c4fee544add968937ae68ac2b5b505797e19234f
Jasmine Nowakowska <18645220+jassycode@users.noreply.github.com>
2022-08-04 21:33:23 +0100

    add ipv4 and ipv6 addresses to front end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
 
+function GetIP(props) {
+  let url = ""
+  let ipAddr = ""
+  if (props.type == 'IPV4') {
+    url = "https://api.ipify.org?format=json";
+    console.log('4');
+  } else if (props.type == 'IPV6') {
+    url = "https://api64.ipify.org?format=json";
+    console.log('6');
+  }
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(url);
+      console.log(data.ip);
+      ipAddr = data.ip;
+      if (!document.getElementsByClassName('IPsection')[0].innerHTML.includes('IPV6')) {
+      document.getElementsByClassName('IPsection')[0].innerHTML += '<h1>' + props.type + ': ' + ipAddr + '</h1>';
+      };
+    }
+    );
+
+}
+
+
 function App() {
-  const banner = <h1>Sextant App</h1>
-      const exhibit = (
-      <div>
-        <h1>Hello!</h1>
-        <h2>Good to see you here.</h2>
-      </div>
-      );
+  const banner = <h1>Sextant App</h1>;
+  const exhibit = (
+    <div className='IPsection'>
+      {<GetIP type='IPV4' />}
+      {<GetIP type='IPV6' />}
+    </div>
+  );
   return (
     <div className="App">
       {banner}
